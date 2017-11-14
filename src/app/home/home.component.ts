234c327e7e6613323c2a685f6754444e32975d72
Yeison Molina <yam.music@hotmail.com>
2017-11-14 09:36:48 -0500

    Using app-list-music component on Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,21 +9,11 @@ import { NavbarService } from './../_services/navbar.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public bricks: Array<{title: string, type: number}>;
 
   constructor(
     public nav: NavbarService,
     private router: Router
-  ) {
-    this.bricks = [
-      {title: 'Brick 1', type: 3},
-      {title: 'Brick 2', type: 1},
-      {title: 'Brick 3', type: 2},
-      {title: 'Brick 4', type: 3},
-      {title: 'Brick 5', type: 2},
-      {title: 'Brick 6', type: 2}
-    ];
-  }
+  ) { }
 
   ngOnInit() {
     $("nav").ready(() => {
