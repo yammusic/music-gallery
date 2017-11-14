08c1c12be8f7a28004e4c75b1a30b6dcfaeda3bf
Yeison Molina <yam.music@hotmail.com>
2017-11-14 01:58:25 -0500

    Adding new services and components for use in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MasonryModule } from 'angular2-masonry';
+import { MaterializeModule } from 'ng2-materialize';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
+import { NavbarService } from './_services/navbar.service';
+import { AuthenticationService } from './_services/authentication.service';
+import { AppRouteModule } from './app-route.routing';
+import { UsersModule } from './users/users.module';
+import { MusicModule } from './music/music.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MasonryModule,
+    MaterializeModule.forRoot(),
+    Ng2SearchPipeModule,
+    UsersModule,
+    MusicModule,
+    AppRouteModule
   ],
-  providers: [],
+  providers: [
+    NavbarService,
+    AuthenticationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
