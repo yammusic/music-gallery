3b403ea740db073702bac1d98562f4bef4fc1ec5
Yeison Molina <yam.music@hotmail.com>
2017-11-17 09:44:55 -0500

    Adding new routes for profile user and edit user

--- src/app/users/users-route.routing.ts
+++ src/app/users/users-route.routing.ts
@@ -3,15 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EditComponent } from './edit/edit.component';
 
 const usersRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  // { path: "user", component: CerebroComponent, children: [
-    // {path: '', component: CerebroListComponent},
-    // {path: ':id', component: CerebroShowComponent},
-    // {path: ':id/edit', component: CerebroFormComponent}
-  // ]}
+  { path: 'profile', component: ProfileComponent },
+  { path: 'profile/edit', component: EditComponent }
 ];
 
 @NgModule({
