7d53e064bd0a5b01e1a887c4f5f564a8d63539f2
Yeison Molina <yam.music@hotmail.com>
2017-11-17 09:43:05 -0500

    Adding modal for delete music on list music component

--- src/app/music/list/list.component.ts
+++ src/app/music/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MzToastService } from 'ng2-materialize';
+
 import { NavbarService } from '../../_services/navbar.service';
 import { MusicService } from '../../_services/music.service';
 
@@ -9,20 +11,42 @@ import { MusicService } from '../../_services/music.service';
   styleUrls: ["./list.component.scss"]
 })
 export class ListMusicComponent implements OnInit {
+  public modalComponent;
   public masonryOptions;
   public loading: boolean;
+  public submitted: boolean;
   public dataFinished: boolean;
   public currentPage: number;
   public musicCollection: Array<any> = [];
+  public modal = {
+    title: `Modal Title`,
+    content: `Modal Content`,
+    musicModel: null
+  };
 
-  constructor(public nav: NavbarService, public music: MusicService) {
+  public modalOptions: Materialize.ModalOptions = {
+    dismissible: false, // Modal can be dismissed by clicking outside of the modal
+    opacity: 0.5, // Opacity of modal background
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: "100%", // Starting top style attribute
+    endingTop: "10%", // Ending top style attribute
+    ready: (modal, trigger) => { }, // Callback for Modal open. Modal and trigger parameters available.
+    complete: () => { } // Callback for Modal close
+  };
+
+  constructor(
+    public nav: NavbarService,
+    public music: MusicService,
+    private toast: MzToastService
+  ) {
     this.loading = true;
     this.currentPage = 1;
     this.getCollection();
   }
 
   ngOnInit() {
-    this.masonryOptions = {transitionDuration: "0.7s"};
+    this.masonryOptions = { transitionDuration: "0.7s" };
   }
 
   public onScroll() {
@@ -37,7 +61,7 @@ export class ListMusicComponent implements OnInit {
   }
 
   public getCollection() {
-    return this.music.getItems({page: this.currentPage}).subscribe(data => {
+    return this.music.getItems({ page: this.currentPage }).subscribe(data => {
       this.loading = false;
       this.musicCollection = this.musicCollection.concat(data);
 
@@ -45,5 +69,35 @@ export class ListMusicComponent implements OnInit {
         this.dataFinished = true;
       }
     });
+  }
+
+  public onModalDelete(modal, music) {
+    this.modalComponent = modal;
+    this.modal.title = `Delete Music: ${music.title} ?`;
+    this.modal.content = `Are you sure want to delete this music: "${music.title}" ?.`;
+    this.modal.musicModel = music;
+    this.modalComponent.open();
+  }
+
+  public onDelete() {
+    this.submitted = true;
+
+    this.music.delete(this.modal.musicModel.id).subscribe(
+      res => {
+        this.toast.show(res.text(), 3000);
+
+        this.submitted = false;
+        this.modalComponent.close();
+        this.musicCollection = [];
+        this.loading = true;
+        this.currentPage = 1;
+        this.dataFinished = false;
+        this.getCollection();
+      },
+      err => {
+        this.submitted = false;
+        this.modalComponent.close();
+      }
+    );
   }
 }
