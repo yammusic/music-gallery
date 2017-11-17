6c1b4947338a592d5b6b05e0812d1f4040efc054
Yeison Molina <yam.music@hotmail.com>
2017-11-17 09:37:46 -0500

    Adding new route for edit music

--- src/app/music/music-route.routing.ts
+++ src/app/music/music-route.routing.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AddMusicComponent } from './add/add.component';
+import { EditMusicComponent } from './edit/edit.component';
 
 const musicRoutes: Routes = [
   { path: 'add', component: AddMusicComponent },
+  { path: 'edit/:id', component: EditMusicComponent }
   // { path: "user", component: CerebroComponent, children: [
     // {path: '', component: CerebroListComponent},
     // {path: ':id', component: CerebroShowComponent},
